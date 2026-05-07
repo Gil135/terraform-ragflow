080d7b021335f0e5050436ff3c0e0675d82dea6c
Gilvan <gilvansou@gmail.com>
2026-05-07 15:01:30 -0300

    commit initial

---
+++ import.tf
@@ -0,0 +1,5 @@
+# import {
+#    to = aws_instance.ragflow_instance
+#    id = "i-0dcce56e4e1435f55"
+  
+#  }
---
+++ main.tf
@@ -0,0 +1,57 @@
+terraform {
+  required_providers {
+    aws={
+        source = "hashicorp/aws"
+        version = ">= 4.16"
+            }
+  }
+  required_version = ">= 1.2.0"
+}
+
+resource "aws_instance" "ragflow_instance" {
+
+  ami = "ami-091138d0f0d41ff90"
+  instance_type                        = "t2.xlarge"
+  region                               = "us-east-1"
+  secondary_private_ips                = []
+  security_groups                      = ["SG RAGFlow"]
+  # iam_instance_profile        = "role-acesso-ssm"
+
+  # depends_on = [
+  #   aws_iam_instance_profile.role-acesso-ssm
+  # ]
+
+  
+tags = {
+  ambiente ="DEV"
+  Name = "Ragflow-DEV"
+  
+}
+
+
+tenancy                           = "default"
+user_data                         = base64encode(file("user-data.sh")) # Instala Docker + RAGFlow v0.24.0
+volume_tags                       = null
+vpc_security_group_ids             = ["sg-02ae2cd21f022a811"]
+
+
+capacity_reservation_specification {
+  capacity_reservation_preference = "open"
+}
+# cpu_options {
+#   core_count       = 4
+#   threads_per_core = 1
+# }
+
+ root_block_device {
+    delete_on_termination = true
+    encrypted             = false
+    iops                  = 3000
+    tags                  = {}
+    tags_all              = {}
+    throughput            = 125
+    volume_size           = 10
+    volume_type           = "gp3"
+  }
+}
+
